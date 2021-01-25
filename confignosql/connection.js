1d99534e870927bc1d24813a1ef268e9292a7e59
a12013143 <a12013143@unet.univie.ac.at>
2021-01-25 04:17:46 +0100

    - Connection, initial user insert (add other insertions later) - migrate as a separate function, currently called outside function

--- confignosql/connection.js
+++ confignosql/connection.js
@@ -1,34 +1,81 @@
 // Connect to SQLite
-const sqlite3 = require("sqlite3").verbose();
+const mongoose = require("mongoose");
+const User = require("../modelsnosql/user");
+const Pet = require("../modelsnosql/pet");
+const PetCategory = require("../modelsnosql/pet_category");
+const Article = require("../modelsnosql/article");
+const ArticleCat = require("../modelsnosql/article_cat");
+const Analytics = require("../modelsnosql/analytics");
 const migration = require("../migration/migration.js");
 
 
-
-
-// G -- Test  mongo ------------------
-// var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient
 var url = "mongodb://localhost:27017/mydb";
-var db;
-MongoClient.connect(url, function(err, db) {
+mongoose.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Mongo connected!");
-  db=db;
   //db.close();//?
 });
 
 (async function() {
   // let url = "mongodb://localhost:27017/mydb"
   let sqlitePath = "./data/db.db";
-  migration.initiate(sqlitePath, url);
   await migration.migrate(sqlitePath, url);
   console.log('OVER');
 })();
 
 
 const connection = {
-  db: db,
+  db:  mongoose.connection,
   initialInsert: function(){  
+    console.log("MongoDB initial insertion.");
+
+    var objs = [
+      {
+        _id:1,
+        name:"Grese Hyseni",email:"hysenigresa@gmail.com",phone:"067239232",address:"Spengergasse",role:"Admin",profile_img_url:"/images/repo/user.png",
+      },
+      {
+        _id:2,
+        name:"Hannah Poor",email:"hannahpoor@gmail.com",phone:"067239232",address:"Maria Hilf.",role:"Admin",profile_img_url:"/images/repo/user.png",
+      }
+    ];
+    mongoose.model('user').insertMany(objs,{ forceServerObjectId: false }).then(function(){ 
+      console.log("Data inserted")  // Success 
+    }).catch(function(error){ 
+      console.log(error)      // Failure 
+    });
+
+    
+    // Article Categories
+    //ID INT UNIQUE PRIMARY KEY, name TEXT, description TEXT 
+ 
+
+    
+
+    //Articles
+    //ID,name TEXT, author TEXT, short_desc TEXT, description TEXT, userID INT, created_at TEXT, updated_at TEXT, article_catID INT
+
+    
+
+    // Pet Categories
+    //ID INT UNIQUE PRIMARY KEY, name TEXT, description TEXT 
+
+    
+    
+    // Pets
+    //ID , ownerID INT, name TEXT, address TEXT, categoryID INT, neutered INT, age_year INT, age_month INT, short_desc TEXT, description TEXT, profile_img_url 
+    // Adoption
+    // ID, userID INT, petID INT, description TEXT, status 
+
+
+
+    // Favourite
+    //ID,userID INT, petID INT,
+
+    
+
+  },
+  migrateFromSqlite: function(){  
     (async function() {
       // let url = "mongodb://localhost:27017/mydb"
       let sqlitePath = "./data/db.db";
