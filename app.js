47c02cbbfba2534c282ad1c41a34a80707a9c5b7
a12013143 <a12013143@unet.univie.ac.at>
2021-01-26 09:29:54 +0100

    - Error redirect to error route, and then handles error (to prevent urls with multiple paths)

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.redirect('/error');
 });
 
 // error handler
@@ -69,7 +69,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500 );
   res.render('error');
 });
 
